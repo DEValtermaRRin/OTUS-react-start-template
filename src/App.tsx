e87f3a001ff8b77d1e383aff31cc06ea30e03547
altermaRRin <dev.altermarrin@gmail.com>
2023-12-05 22:45:02 +0300

    about me added

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,37 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Я - Марина) <br />
+          считаю себя начинающим web-разработчиком, хотя на React написала 4 приложения, да и в разработке уже года 2,
+          но все равно есть сомнения
+          <br />
+          Цель - уверенно создавать приложения на ReactJS. Хочу досконально разобраться в жизненном цикле компонента,
+          реакт хуки, чтобы не допускать утечку данных. Также освоить TypeScript и NextJS. Разобраться со storybook)
+          <br />
+          <br />
+          Технологии, которыми владею:
+          <br />
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>SCSS</li>
+            <li> Семантическая верстка</li>
+            <li>Адаптивная верстка</li>
+            <li>Webpack</li>
+            <li>Gulp</li>
+            <li>Git</li>
+            <li>Jest</li>
+            <li> Node.js</li>
+            <li>MongoDB</li>
+          </ul>
+          Технологии, которые в процессе изучения:
+          <br />
+          <ul>
+            <li>TypeScript</li>
+            <li>StoryBook</li>
+            <li>Redux</li>
+            <li> Redux/Toolkit</li>
+          </ul>
         </p>
       </header>
     </div>
